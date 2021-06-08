b6054878fd31d420182c3ee103cc8690a5c93d9a
Ringo <ry.li@qq.com>
2021-06-09 07:28:51 +0800

    day44-6 路由列表的开发

--- python全栈/day44/framework.py
+++ python全栈/day44/framework.py
@@ -8,7 +8,7 @@ def index():
     response_header = [("Server", "PWS/1.1")]
     # 响应体
     # 1.打开指定模板文件，读取模板文件内容
-    with open("static/index.html", "r") as file:
+    with open("template/index.html", "r") as file:
         file_data = file.read()
     # 2.查询数据库，替换模板中的变量（{%content%}
     # 获取当前时间,模拟数据库数据
@@ -17,6 +17,18 @@ def index():
     response_body = file_data.replace("{%content%}", data)
     # 这里返回的是一个元组，括号可以省略
     return status, response_header, response_body
+
+def center():
+    # 状态信息
+    status = "200 OK"
+    # 响应头信息
+    response_header = [("Server", "PWS/1.1")]
+    # 响应体
+    with open("template/center.html", "r") as file:
+        file_data = file.read()
+
+    # 这里返回的是一个元组，括号可以省略
+    return status, response_header, file_data
 
 def not_found():
     # 状态信息
@@ -29,18 +41,35 @@ def not_found():
     # 这里返回的是一个元组，括号可以省略
     return status, response_header, data 
 
+# 创建路由列表，每条记录是一个路由，由元组构成
+route_list = [
+    ("/index.html", index),
+    ("/center.html", center)
+]
+
 # 处理动态资源请求
 def handle_request(env):
     # 获取动态资源请求路径
     request_path = env["request_path"]
     print("动态资源请求路径：", request_path)
-    # 判断动态资源路径，选择指定函数处理对应的动态资源请求
-    if request_path == "/index.html":
-        # 获取首页数据
-        result = index()
-        # 把处理后的结果返回给web服务器，用于web服务器拼接响应报文使用
-        return result
+    # # 判断动态资源路径，选择指定函数处理对应的动态资源请求
+    # if request_path == "/index.html":
+    #     # 获取首页数据
+    #     result = index()
+    #     # 把处理后的结果返回给web服务器，用于web服务器拼接响应报文使用
+    #     return result
+    # else:
+    #     # 没有找到资源路径，返回404
+    #     result = not_found()
+    #     return result
+    for path, func in route_list:
+        if request_path == path:
+            # 找到路由，执行对应的处理函数
+            # print(request_path)
+            result = func()
+            # 将处理后的结果返回web服务，进行报文拼接
+            return result
     else:
-        # 没有找到资源路径，返回404
+        # 没有资源数据，返回404
         result = not_found()
         return result
