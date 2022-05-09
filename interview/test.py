15c84a9c5a83631023c60b29977c4212c5eb7e38
Ringo <ry.li@qq.com>
2022-05-09 22:54:58 +0800

    第一轮复习完毕

--- interview/test.py
+++ interview/test.py
@@ -1,24 +1,16 @@
-candidates = [2,3,6,7]
-target = 7
-
+nums = [2, 5, 7, 3, 2, 6, 2, 4, 1]
 path = []
-result = []
-# 回溯，在for循环中嵌套递归
-def backtracking(nums, target, startindex):
-    # 1.确定参数，数组，startindex，
-    # 2.确定终止条件和收获返回值
-    if sum(path) > target:
+resault = []
+def backtracking(nums, startindex):
+    if len(path) == 3:
+        if path[0]**3 + path[1]**3 + path[2] == path[0]*100 + path[1]*10 + path[2]:
+            resault.append(path.copy())
         return
-    if sum(path) == target:
-        result.append(path.copy())
+    if len(path) > 3:
         return
-
-    # 3.确定单层逻辑
     for i in range(startindex, len(nums)):
         path.append(nums[i])
-        backtracking(nums, target, i)
-        path.pop()
-
-backtracking(candidates, 7, 0)
-print(result)
-        
+        backtracking(nums, 0)
+        path.pop()   
+backtracking(nums, 0)
+print(resault)
